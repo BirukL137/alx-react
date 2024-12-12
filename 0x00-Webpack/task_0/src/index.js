b5379226ca42a0fe3f371713682c69e0b4631aa3
BirukL137 <buraf13k@gmail.com>
2024-12-12 14:03:49 +0300

    0. Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
-var $ = require( "jquery" );
+import $ from 'jquery';
 
-$(function() {
-  $("body").append("<p>ALX Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<p>Copyright - ALX</p>");
+$(document).ready(function() {
+  $('body').append('<p>ALX Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - ALX</p>');
 });
