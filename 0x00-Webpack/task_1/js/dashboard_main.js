b7c785031812f03848935d5e10329d893f4dea0c
BirukL137 <buraf13k@gmail.com>
2024-12-13 11:13:51 +0300

    1. Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,20 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-$(document).ready(function () {
-  $(body).append('<p>ALX Dashboard</p>');
-  $(body).append('<p>Dashboard data for the students</p>');
-  $(body).append('<button>Click here to get started</button>');
-  $(body).append("<p id='count'></p>");
-  $(body).append('<p>Copyright - ALX');
+$(document).ready(function() {
+  $('body').append('<p>ALX Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append("<p id='count'></p>");
+  $('body').append('<p>Copyright - ALX');
 
   let count = 0;
 
   function updateCounter() {
     count++;
-    $('#count').text(`{count} clicks on the button`);
-  };
+    $('#count').text(`${count} clicks on the button`);
+  }
 
-  $('button').on('click', _debounce(updateCounter));
+  const debouncedUpdateCounter = _.debounce(updateCounter);
+  $('button').on('click', debouncedUpdateCounter);
 });
