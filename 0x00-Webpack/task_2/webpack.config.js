a855cdc4d800ba4dae80fd61695287996c70140e
BirukL137 <buraf13k@gmail.com>
2024-12-17 12:21:10 +0300

    2. Adding CSS & Images

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,34 +1,32 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./js/dashboard_main.js", // Entry file
+  entry: './js/dashboard_main.js',
   output: {
-    filename: "bundle.js", // Output JS file
-    path: path.resolve(__dirname, "public"), // Output directory
-    assetModuleFilename: "assets/[name][ext]", // Images output folder
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public'
   },
-  mode: "production", // Set production mode
+  mode: 'production',
   module: {
-    rules: [
-      {
-        test: /\.css$/, // Match CSS files
-        use: ["style-loader", "css-loader"], // Process CSS files
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i, // Match image files
-        type: "asset/resource", // Include images in assets folder
-        use: [
-          {
-            loader: "image-webpack-loader", // Optimize images
-            options: {
-              mozjpeg: { progressive: true, quality: 75 },
-              optipng: { enabled: true },
-              pngquant: { quality: [0.65, 0.9], speed: 4 },
-              gifsicle: { interlaced: false },
-            },
-          },
-        ],
-      },
-    ],
-  },
+	rules: [
+		{
+			test: /\.css$/i,
+			use: ["css-loader", "style-loader"],
+		},
+		{
+			test: /\.(gif|png|jpe?g|svg)$/i,
+			use: [
+				"file-loader",
+				{
+					loader: "image-webpack-loader",
+					options: {
+							bypassingOnDebug: true,
+							disable: true,
+					},
+				},
+			],
+		},
+	],
+},
 };
