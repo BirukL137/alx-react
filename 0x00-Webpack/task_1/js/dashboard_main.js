b4833723cc751d76af424323bdad74dc62e77b9a
BirukL137 <buraf13k@gmail.com>
2024-12-17 10:20:23 +0300

    1. Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
   $('body').append('<p>ALX Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
-  $('body').append("<p id='count'></p>");
-  $('body').append('<p>Copyright - ALX');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - ALX</p>');
 
   let count = 0;
 
