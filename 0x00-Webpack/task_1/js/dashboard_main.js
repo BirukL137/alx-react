2b98d12d9bd7a30741e8fefac8218c34e3fdfe92
BirukL137 <buraf13k@gmail.com>
2024-12-17 11:42:27 +0300

    1. Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,25 @@
+// Import jQuery and Lodash
 import $ from 'jquery';
 import _ from 'lodash';
-$(function() {
+
+// Initialize the count variable
 let count = 0;
 
-$('body').append('<p>Holberton Dashboard</p>');
+// Function to update the counter
+function updateCounter() {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+// Add the DOM elements
+$('body').append('<p>ALX Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - ALX</p>');
 
-// $('button').on('click', updateCounter);
-$('button').on('click', _.debounce(updateCounter, 500));
+// Debounce the updateCounter function
+const debouncedUpdateCounter = _.debounce(updateCounter, 300);
 
-function updateCounter() {
-  count = count + 1;
-  $('#count').text(count + ' clicks on the button');
-}
-});
+// Bind the debounced function to the button click event
+$('button').on('click', debouncedUpdateCounter);
